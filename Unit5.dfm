8bd5eb3bde5c524a1654668b47f8ec8bb294a89a
UserMelancholic <sergii.bilobrovyk@gmail.com>
2018-06-29 00:55:24 +0300

    task_v1

---
+++ Unit5.dfm
@@ -0,0 +1,64 @@
+object Form5: TForm5
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu, biMinimize]
+  BorderStyle = bsSingle
+  Caption = #1057#1087#1080#1089#1086#1082' '#1085#1072#1079#1074#1072#1085#1080#1081' '#1079#1072#1076#1072#1095
+  ClientHeight = 390
+  ClientWidth = 386
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Memo1: TMemo
+    Left = 26
+    Top = 24
+    Width = 313
+    Height = 249
+    Lines.Strings = (
+      '')
+    ScrollBars = ssVertical
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 40
+    Top = 296
+    Width = 75
+    Height = 25
+    Caption = #1047#1072#1075#1088#1091#1079#1080#1090#1100
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 152
+    Top = 296
+    Width = 75
+    Height = 25
+    Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 264
+    Top = 296
+    Width = 75
+    Height = 25
+    Caption = #1054#1095#1080#1089#1090#1080#1090#1100
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 88
+    Top = 344
+    Width = 209
+    Height = 25
+    Caption = #1055#1077#1088#1077#1079#1072#1076#1072#1090#1100' '#1089#1087#1080#1089#1086#1082' '#1076#1083#1103' '#1087#1083#1072#1085#1080#1088#1086#1074#1097#1080#1082#1072
+    TabOrder = 4
+    OnClick = Button4Click
+  end
+end
---
+++ Unit5.pas
@@ -0,0 +1,69 @@
+unit Unit5;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtDlgs;
+
+type
+  TForm5 = class(TForm)
+    Memo1: TMemo;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form5: TForm5;
+
+implementation
+uses Unit1;
+
+{$R *.dfm}
+
+// Загрузить файл в Memo1
+procedure TForm5.Button1Click(Sender: TObject);
+begin
+//  if OpenTextFileDialog1.Execute then memo1.Lines.LoadFromFile(OpenTextFileDialog1.FileName)
+  if FileExists( ExtractFilePath(ParamStr(0))+'Data\TaskList.txt' ) then begin  // проверка существования файла
+    Memo1.Lines.LoadFromFile( ExtractFilePath(ParamStr(0))+'Data\TaskList.txt' )
+  end;
+end;
+
+// Сохранить содержимое Memo1 в файл
+procedure TForm5.Button2Click(Sender: TObject);
+begin
+//if SaveTextFileDialog1.Execute then
+//    Memo1.Lines.SaveToFile( SaveTextFileDialog1.FileName );
+  if Application.MessageBox('Вы уверены, что хотите записать в файл этот список задач ?', 'Внимание', MB_ICONQUESTION+MB_YESNO+MB_DEFBUTTON2)=idYes then
+    Memo1.Lines.SaveToFile( ExtractFilePath(ParamStr(0))+'Data\TaskList.txt' );
+end;
+
+procedure TForm5.Button3Click(Sender: TObject);
+begin
+  // Вывод запроса на подтверждение очистки списка
+// MessageBox(handle, PChar('текст'),PChar('заголовок'), MB_ICONSTOP+MB_YESNOCANCEL+MB_DEFBUTTON2);
+  if Application.MessageBox('Вы уверены, что хотите очистить список ?', 'Внимание', MB_ICONQUESTION+MB_YESNO+MB_DEFBUTTON2)=idYes then
+    Memo1.Clear;
+end;
+
+// Перезадать список для планировщика по текущему содержимому Memo1
+procedure TForm5.Button4Click(Sender: TObject);
+begin
+  // Перезададим список для выбора значений в поле 'Задача' (2-я колонка, нумерация с 0)
+  Form1.DBGrid1.Columns[3].PickList := Memo1.Lines;
+  ShowMessage('Список названий задач для планировщика был перезадан');
+end;
+
+
+end.
